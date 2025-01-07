052bd91ce41b9751876cc05900cb6aafaf2a3965
Pogoretskiy777 <jpogoretskiy@gmail.com>
2025-01-07 10:53:49 -0500

    Added reload utility command

--- index.js
+++ index.js
@@ -28,4 +28,18 @@ for (const folder of commandFolders) {
   }
 }
 
+// Retrieve all events
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
 client.login(token);
