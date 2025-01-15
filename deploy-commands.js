abbc64224a5dc7f36e5285095941e13f02602801
Pogoretskiy777 <jpogoretskiy@gmail.com>
2025-01-15 13:26:36 -0500

    Added global deployment

--- deploy-commands.js
+++ deploy-commands.js
@@ -37,7 +37,7 @@ const rest = new REST().setToken(token);
 
     // The put method is used to fully refresh all commands in the guild with the current set
     // Remove 'guildId' to make global deployment
-    const data = await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+    const data = await rest.put(Routes.applicationCommands(clientId), {
       body: commands,
     });
 
